f3d27b70dc23dc4bebe0abedf2518c5d140e5c10
Paul Chen <pochuan@stanford.edu>
2011-11-23 20:42:15 -0800

    Features fbank

--- LDCdata/htktocsvmanyfiles.m
+++ LDCdata/htktocsvmanyfiles.m
@@ -1,4 +1,4 @@
-function [] = htktocsvmanyfiles( filenamesFiles )
+function [] = htktocsvmanyfiles(filenamesFiles, outputFile)
 % FilenameFiles is a list of the names of files containing 
 % the filenames to be parsed
 % Based on function written by Mark Hasegawa-Johnson
@@ -7,28 +7,40 @@ function [] = htktocsvmanyfiles( filenamesFiles )
 %
 
 more off;
+dlmwrite(outputFile, []);
+filenamesFiles
 for filenamesFileInd=1:size(filenamesFiles,1)
   ['reading files of class', filenamesFileInd-1]
-  filesFid = fopen(filenamesFiles(filenamesFileInd,:),'r','b');
+  filename = filenamesFiles{filenamesFileInd}
+  size(filename, 1)
+  size(filename, 2)
+  filesFid = fopen(filename,'r');
+
+  'passed here'
   if filesFid<0,
     error(sprintf('Unable to read from file %s',filenamesFiles(filenamesFileInd,:)));
   end
-  files = fscanf(filesFid, '%[^\n]');
-  for fileInd=1:size(files,1)
-    file = files(fileInd,:);
-    ['reading file', file]
+  files = textscan(filesFid, '%s');
+  'passed here too'
+  for fileInd=1:size(files{1,1},1)
+    'here also'
+    file = files{1,1}{fileInd,1};
+    'me too'
+    %['reading file', file]
     size(file,1) 
     size(file,2)
-    [DATA, HTKCode] = htkread(file(1,1:(size(file,2)-1)));
+    file
+    'righto'
+    [DATA, HTKCode] = htkread(file);
     'done'
 
     AVGDATA = repmat(0, (int32(floor(size(DATA, 1)/160))), (size(DATA, 2)));
     'here'
     for rowNum = 1:(size(DATA, 1)- mod(size(DATA, 1), 160))
       avgRowNum = int32(floor((rowNum-1)/160))+1;
-      rowNum
+      %rowNum
       for col=1:size(DATA,2) 
-        col
+        %col
         AVGDATA(avgRowNum,col) = AVGDATA(avgRowNum,col) + DATA(rowNum,col);
       end
     end
@@ -37,7 +49,7 @@ for filenamesFileInd=1:size(filenamesFiles,1)
     'uhuh'
     classVec = repmat(filenamesFileInd-1,[size(AVGDATA, 1) 1]);
     'yep'
-    dlmwrite('features_test.csv', [AVGDATA classVec], '-append');
+    dlmwrite(outputFile, [AVGDATA classVec], '-append');
     'ok'
   end
   fclose(filesFid);
