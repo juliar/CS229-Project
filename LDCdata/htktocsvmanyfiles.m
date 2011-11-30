1e576f6f30b8c3385d6fc6849bb2176a235dd673
Paul Chen <pochuan@stanford.edu>
2011-11-30 02:13:18 -0800

    with coordinate std

--- LDCdata/htktocsvmanyfiles.m
+++ LDCdata/htktocsvmanyfiles.m
@@ -13,12 +13,16 @@ bucketSize = 4;
 sliding = 0;
 % slidingDist in seconds
 slidingDist = 1;
+% Coordinate Standardization
+standardize = 1;
 
 
 more off;
 % This assumes feature windows of 0.025 seconds. Which means theres 40 features per second.
 bucketConst = bucketSize*40;
 slidingConst = slidingDist*40;
+
+if 0
 
 filenamesFiles
 for filenamesFileInd = 1:size(filenamesFiles,1)
@@ -45,18 +49,12 @@ for filenamesFileInd = 1:size(filenamesFiles,1)
     
     [DATA, HTKCode] = htkread(file);
     %DATA
-    sum = 0;
-    for i = 1:bucketConst
-      sum = sum + DATA(i,1);
-    end
-    result = sum/bucketConst
-    
-
 
     if (sliding)
       'Sliding Buckets'
       AVGDATA = repmat(0, (int32(floor(size(DATA, 1)/(slidingConst))) - bucketSize + 1), size(DATA, 2));
       for slide = 1:(int32(floor(size(DATA, 1)/(slidingConst))) - bucketSize + 1)
+
         for slideNum = 0:bucketConst-1
           for col = 1:size(DATA,2)
             AVGDATA(slide, col) = AVGDATA(slide, col) + DATA((slide + slideNum), col);
@@ -78,18 +76,97 @@ for filenamesFileInd = 1:size(filenamesFiles,1)
     AVGDATA = AVGDATA ./ bucketConst;
     
     classVec = repmat(filenamesFileInd-1,[size(AVGDATA, 1) 1]);
-    
-    if filenamesFileInd == 1 && fileInd == 1
-      dlmwrite(outputTrainFile, 1:(size(AVGDATA,2))+1);
-      dlmwrite(outputTestFile, 1:(size(AVGDATA,2))+1);
 
-    end
-    if fileInd < size(files{1,1},1)*0.7
-      dlmwrite(outputTrainFile, [AVGDATA classVec], '-append');
+    if(standardize)
+        
+
+      if fileInd < size(files{1,1},1)*0.7
+        dlmwrite('outputTrainTemp', [AVGDATA classVec], '-append');
+      else
+        dlmwrite('outputTestTemp', [AVGDATA classVec], '-append');
+      end
+
+
+
     else
-      dlmwrite(outputTestFile, [AVGDATA classVec], '-append');
+      if filenamesFileInd == 1 && fileInd == 1
+        dlmwrite(outputTrainFile, 1:(size(AVGDATA,2))+1);
+        dlmwrite(outputTestFile, 1:(size(AVGDATA,2))+1);
+
+      end
+      if fileInd < size(files{1,1},1)*0.7
+        dlmwrite(outputTrainFile, [AVGDATA classVec], '-append');
+      else
+        dlmwrite(outputTestFile, [AVGDATA classVec], '-append');
+      end
     end
   end
   fclose(filesFid);
+
   []
+end
+
+end
+
+
+if (standardize)
+  'Starting Standardization'
+  'Reading tempfiles'
+  trainTemp = csvread('outputTrainTemp');
+  numTrainRows = size(trainTemp,1);
+  testTemp = csvread('outputTestTemp');
+  numTestRows = size(testTemp,1);
+
+  temp = vertcat (trainTemp, testTemp); %Concatinate train on top
+
+
+  [numRows, numCols] = size(temp);
+  numCols = numCols-1; % To not standardize the label
+
+  % Calculate mean
+  mean = repmat(0, 1, numCols);
+  for row = 1:numRows
+    for col = 1:numCols
+      mean(1,col) = mean(1,col) + temp(row,col);
+    end
+  end
+  mean = mean ./ numRows;
+
+  % Calculate stddev
+  stddev = repmat(0, 1, numCols);
+  for row = 1:numRows
+    for col = 1:numCols
+      stddev(1,col) = stddev(1,col)+(temp(row,col) - mean(1,col))*(temp(row,col) - mean(1,col));
+    end
+  end
+  stddev = stddev ./ numRows;
+  stddev = sqrt(stddev)
+
+  % Calculate coordinate standardization
+  for row = 1:numRows
+    for col = 1:numCols
+      temp(row,col) = (temp(row,col) - mean(1,col))/stddev(1,col);
+    end
+  end
+
+
+  %write index on first row
+  dlmwrite(outputTrainFile, 1:(numCols + 1));
+  dlmwrite(outputTestFile, 1:(numCols + 1));
+
+  Train = zeros(numTrainRows, numCols + 1);
+  Test = zeros(numRows - numTrainRows, numCols + 1);
+
+  for i = 1:numRows
+    if i < numTrainRows + 1
+          Train(i,:) = temp(i,:);
+    else
+          Test(i-numTrainRows,:) = temp(i,:);
+    end
+  end
+  dlmwrite(outputTrainFile, Train, '-append');
+  dlmwrite(outputTestFile, Test, '-append');
+
+
+  'Finished Standardization'
 end
